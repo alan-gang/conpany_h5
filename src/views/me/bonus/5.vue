d7d47561d5d63006510c5ce594afb91e5c524da0
xinyou <646733518@qq.com>
2019-05-14 19:21:52 +0800

    彩票奖金

---
+++ src/views/me/bonus/5.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="section">
+      <ul>
+        <li>
+          <span>玩法：61888888</span>
+          <span class="right">返点：6.1</span>
+        </li>
+        <li>
+          <span>奖级：7.1</span>
+          <span class="right">奖金：50000</span>
+        </li>
+        <li>
+          <span>状态：使用中</span>
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <ul>
+        <li>
+          <span>玩法：61888888</span>
+          <span class="right">返点：6.1</span>
+        </li>
+        <li>
+          <span>奖级：7.1</span>
+          <span class="right">奖金：50000</span>
+        </li>
+        <li>
+          <span>状态：使用中</span>
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <ul>
+        <li>
+          <span>玩法：61888888</span>
+          <span class="right">返点：6.1</span>
+        </li>
+        <li>
+          <span>奖级：7.1</span>
+          <span class="right">奖金：50000</span>
+        </li>
+        <li>
+          <span>状态：使用中</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import config from '@/config'
+export default {
+  mixins: [config],
+  components: {
+  },
+  name: 'bonus_5',
+  props: [],
+  computed: {
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~src/css/var.stylus'
+// 建议不添加scoped， 所有样式最多嵌套2层
+.container
+  margin-top 1.08rem
+  .section
+    width 7.07rem
+    margin 0.23rem auto 0
+    padding 0.55rem 0
+    background-color #fff
+    border-radius 0.07rem
+    ul
+      width 6.31rem
+      margin 0 auto
+      padding 0 0.2rem
+      li
+        position relative
+        height 0.55rem
+        line-height 0.55rem
+        padding 0 0.2rem
+        .right
+          position absolute
+          left 3.5rem
+        &:nth-child(odd)
+          background-color #efefef
+</style>
--- src/views/me/bonus/index.vue
+++ src/views/me/bonus/index.vue
@@ -1,22 +1,61 @@
 <template lang="pug">
+f7-page.bonus(:page-content="false")
+  f7-navbar(title="奖金详情" back-link)
+  div.head
+    a._icon._bonusicon.link.color-black
+      i.icon.f7-icons home
+    br
+    strong it0k011
+    br
+    span 昵称：浆糊点击
 
-f7-page
-  f7-navbar(title="彩票奖金" back-link)
+  f7-toolbar(tabbar top scrollable style="transform: none")
+    f7-link(tab-link tab-link-active href="#bonus_1")
+      f7-button 快投30秒
+    f7-link(tab-link href="#bonus_2" )
+      f7-button 腾讯分分彩
+    f7-link(tab-link href="#bonus_3" )
+      f7-button 新疆时时彩
+    f7-link(tab-link href="#bonus_4" )
+      f7-button 上海时时乐
+    f7-link(tab-link href="#bonus_5" )
+      f7-button 北京PK10
 
-
-
+  f7-tabs(animated swipeable)
+    f7-tab#bonus_1(tab-active @tab:show=" __tabShow ")
+      bonus1
+    f7-tab#bonus_2(@tab:show=" __tabShow ")
+      bonus2
+    f7-tab#bonus_3(@tab:show=" __tabShow ")
+      bonus3
+    f7-tab#bonus_4(@tab:show=" __tabShow ")
+      bonus4
+    f7-tab#bonus_5(@tab:show=" __tabShow ")
+      bonus5
 </template>
 
 <script>
 import config from '@/config'
+import bonus1 from '@/views/me/bonus/1'
+import bonus2 from '@/views/me/bonus/2'
+import bonus3 from '@/views/me/bonus/3'
+import bonus4 from '@/views/me/bonus/4'
+import bonus5 from '@/views/me/bonus/5'
+// import api from '@/api'
 export default {
   mixins: [config],
   components: {
+    bonus1,
+    bonus2,
+    bonus3,
+    bonus4,
+    bonus5
   },
-  name: 'load',
+  name: 'bonus',
   props: [],
   data () {
-    return {}
+    return {
+    }
   },
   created () {
   },
@@ -28,4 +67,33 @@ export default {
 <style lang="stylus">
 @import '~src/css/var.stylus'
 // 建议不添加scoped， 所有样式最多嵌套2层
+  .bonus
+    .head
+      margin-top 0.8rem
+      height 2.8rem
+      background linear-gradient(to right, #ff8131, #ff5429)
+      text-align center
+      ._bonusicon
+        margin-top 0.7rem
+        transform scale(4)
+      strong
+        display inline-block
+        font-size 0.27rem
+        color #fff
+        margin-top 0.55rem
+      span
+        font-size 0.22rem
+        color #ffb296
+    .toolbar
+      margin-top 2.7rem
+      .tab-link
+        .button
+          font-size 0.27rem
+          color #333
+        &.tab-link-active
+          .button
+            color #ff5429
+            border-bottom 0.05rem solid #ff5429
+            border-radius 0
+            padding-bottom 0.2rem
 </style>
