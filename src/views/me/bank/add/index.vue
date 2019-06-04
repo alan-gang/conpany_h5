2ba09ced5f99f2c5066a155105443281220ce7ae
Wilon <wilon.di@tech-core.org>
2019-06-04 11:30:04 +0800

    银行卡

--- src/views/me/bank/add/index.vue
+++ src/views/me/bank/add/index.vue
@@ -1,59 +1,143 @@
-<template>
-  <f7-page class="add">
-    <f7-navbar title="添加银行卡" back-link></f7-navbar>
-    <form>
-      <div class="section">
-        <label for="name">持卡人</label>
-        <input type="text" placeholder="请输入持卡人姓名" id="name" />
-      </div>
-      <div class="section">
-        <label for="bankNo">银行卡号</label>
-        <input type="text" placeholder="请输入银行卡号" id="bankNo" maxlength="19" />
-      </div>
-      <div class="section">
-        <label>银行名称</label>
-        <select>
-          <option class="option">请选择银行名称</option>
-          <option>小明的爸爸是谁？</option>
-          <option>小明的爸爸的爸爸叫什么？</option>
-          <option>小明是垃圾堆里捡来的么？</option>
-          <option>为什么小明管隔壁老王叫叔叔？</option>
-        </select>
-      </div>
-      <div class="section">
-        <label>开户省市</label>
-        <select>
-          <option class="option">请选择开户省市</option>
-          <option>小明的爸爸的爸爸叫什么？</option>
-          <option>小明是垃圾堆里捡来的么？</option>
-          <option>为什么小明管隔壁老王叫叔叔？</option>
-        </select>
-      </div>
-      <div class="section">
-        <label for="branch">开户支行</label>
-        <input type="text" placeholder="请输入开户支行" id="branch" />
-      </div>
-      <a href="javascript:;" class="confirm" @click="confirm">确 认</a>
-    </form>
-  </f7-page>
+<template lang="pug">
+  f7-page.addBank
+    f7-navbar(title="添加银行卡" back-link)
+    form
+      div.section
+        label(for="name") 持卡人
+        input.w_300(type="text" placeholder="由2至8个汉字组成，不能使用数字字符" v-model="realName" ref="name")
+      div.section
+        label(for="cardno") 银行卡号
+        input.w_300(type="number" placeholder="银行卡由16位、18位、19位数字组成" v-model="cardno" ref="cardno" maxlength="19")
+      //- div.section
+      f7-list
+        f7-list-item.c_6(title="银行名称" smart-select :smart-select-params="{openIn: 'sheet'}")
+          select(name="bank-name").c_3
+            option(v-for=" (bank, index) in bankList" :key="index" :class="'_bankicon_'+bank.apiName" v-model="bank.apiName" :value="bank.bankName") {{ bank.bankName }}
+        //- label 银行名称
+        //- select
+        //-   option.option.bankName 请选择银行名称
+        //-   option._icon(v-for=" (bank, index) in bankList" :key="index" :class="'_bankicon_'+bank.apiName" v-model="bank.apiName")
+        //-     i.icon.f7-icons.pl_30 {{ bank.bankName }}
+      div.section
+        label 开户省市
+        //- select
+          //- option.option 请选择开户省市
+        input(type="text" placeholder="请选择开户省市" readonly id="picker-province")
+      div.section
+        label(for="branch") 开户支行
+        input.w_300(type="text" placeholder="请输入开户支行" v-model="branch" id="branch")
+      a(href="javascript:;" class="confirm" @click="bindBankCard") 确 认
 </template>
 
 <script>
   import config from '@/config'
+  import api from '@/api'
   export default {
     mixins: [config],
     components: {
     },
-    name: 'add',
+    name: 'addBank',
     props: [],
     data () {
-      return {}
+      return {
+        bankList: [],
+        branch: '',
+        realName: '',
+        cardno: null,
+        apiName: '',
+        bankSelected: '',
+        allProvinces: {},
+        proList: []
+      }
     },
     created () {
+      this.getBankList()
+    },
+    mounted () {
+      this.picker()
     },
     methods: {
-      confirm () {
-        this.__go('/me/bank/')
+      picker () {
+        this.$.get(api.getProvices).then(({data: {allProvinces}}) => {
+          allProvinces.map(x => {
+            // console.log(x.subRegionList)
+            this.proList.push(x.title)
+            this.allProvinces[x.title] = x.subRegionList.map(y => y.title)
+          })
+
+          // this.allProvinces = {allProvinces.map(x => x.title),allProvinces[index]}
+          // console.log(this.allProvinces['内蒙古'])
+          this.$f7.picker.create({
+            inputEl: '#picker-province',
+            rotateEffect: true,
+            // divider: true,
+            // formatValue (v, dv) {
+            //   return dv.join('-')
+            // },
+            formatValue (picker, values) {
+              return values[1]
+            },
+            cols: [
+              {
+                // value () {
+                //   return this.allProvinces
+                // },
+                values: this.proList,
+                // displayValues: this.allProvinces.map(x => x.title),
+                onChange: function (picker, cityes) {
+                  if (picker.cols[1].replaceValues) {
+                    console.log(cityes)
+                    picker.cols[1].replaceValues(allProvinces.map(x => x.subRegionList))
+                    // picker.cols[1].replaceValues(this.allProvinces.map(x => x.title))
+                  }
+                },
+              },
+              {
+                values: this.allProvinces['北京'],
+                width: 160,
+              }
+              // {
+              //   values: this.allProvinces[0].subRegionList,
+              //   displayValues: this.allProvinces[0].subRegionList.map(x => x.title),
+              // },
+            ],
+          })
+        })
+      },
+      getBankList () {
+        this.$.get(api.getBankList).then(({data}) => {
+          this.bankList = data.allBankData
+          // console.log(this.bankList)
+        })
+      },
+      bindBankCard () {
+        // if (!this.realName || !this.cardno || !this.branch || !this.provinceId || !this.cityId || !this.apiName) {
+        console.log(this.realName + this.cardno + this.branch + this.bankSelected)
+        if (!this.realName || !this.cardno || !this.branch || !this.bankSelected) {
+          this.$f7.dialog.alert('请填写完整资料', '', () => {
+
+          })
+          return
+        }
+        this.$.get(api.bindBankCard, {
+          // provinceId: this.provinceId,
+          // cityId: this.cityId,
+          branch: this.branch,
+          realName: this.realName,
+          cardNo: this.cardno,
+          apiName: this.apiName
+        })
+          .then(res => {
+            if (!res.success) {
+              this.$f7.dialog.alert(res.msg, () => {
+
+              })
+              return
+            }
+            this.$f7.dialog.alert('绑定银行卡成功！', () => {
+              this.__go('/me/bank/')
+            })
+          })
       }
     }
   }
@@ -62,23 +146,29 @@
 <style lang="stylus">
   @import '~src/css/var.stylus'
   // 建议不添加scoped， 所有样式最多嵌套2层
-  .add
+  .addBank
     form
       margin-top 0.18rem
-      font-size 0.27rem
+      font-size 16px
+      .list
+        margin 0 0 .18rem 0
+      .smart-select .item-after
+        width 81%
+        max-width 81%
+        color #333
       .section
-        height 0.92rem
-        line-height 0.92rem
+        height 44px
+        line-height 44px
         margin-bottom 0.18rem
         background-color #fff
         label
           display inline-block
           color #666
-          width 1.15rem
-          padding-left 0.2rem
+          width 1.3rem
+          padding-left 0.25rem
         select
           width 6.35rem
-          margin -0.65rem 0 0 1.15rem
+          margin -0.63rem 0 0 1.25rem
           padding-left 0.25rem
           option
             &.option
@@ -90,14 +180,15 @@
           &:-moz-placeholder { color: #c5c5c5; }
           &::-moz-placeholder { color: #c5c5c5; }
           &:-ms-input-placeholder { color: #c5c5c5; }
-      .confirm
-        display block
-        width 7.07rem
-        height 0.82rem
-        line-height 0.82rem
-        margin 1.38rem auto 0
-        color #fff
-        background linear-gradient(to right, #fe8f48, #ff5429)
-        border-radius 0.05rem
-        text-align center
+
+  .confirm
+    display block
+    width 7.07rem
+    height 0.82rem
+    line-height 0.82rem
+    margin 1.38rem auto 0
+    color #fff
+    background linear-gradient(to right, #fe8f48, #ff5429)
+    border-radius 0.05rem
+    text-align center
 </style>
--- src/views/me/bank/index.vue
+++ src/views/me/bank/index.vue
@@ -1,68 +1,59 @@
-<template>
-  <f7-page class="bank">
-    <f7-navbar title="银行卡" back-link></f7-navbar>
-    <div class="list">
-      <ul>
-        <li class="swipeout" @swipeout:deleted="unbind">
-          <div class="swipeout-content section branch">
-            <div>
-              <a href="javascript:;" class="_icon _bankicon_1 link color-black">
-                <i class="icon f7-icons"> home </i>
-              </a>
-              <span class="type">建设银行储蓄卡</span>
-              <em>● ● ● ● 4113</em>
-            </div>
-            <span class="date">2016/10/15</span>
-          </div>
-          <div class="swipeout-actions-right">
-            <a href="#" data-confirm="银行卡解绑后，不能再绑定到其它帐号上！" data-confirm-title="温馨提示" class="swipeout-delete">删除</a>
-          </div>
-        </li>
-        <li class="swipeout" @swipeout:deleted="unbind">
-          <div class="swipeout-content section branch">
-            <div>
-              <a href="javascript:;" class="_icon _bankicon_2 link color-black">
-                <i class="icon f7-icons"> home </i>
-              </a>
-              <span class="type">农业银行储蓄卡</span>
-              <em>● ● ● ● 4113</em>
-            </div>
-            <span class="date">2016/10/15</span>
-          </div>
-          <div class="swipeout-actions-right">
-            <a href="#" data-confirm="银行卡解绑后，不能再绑定到其它帐号上！" data-confirm-title="温馨提示" class="swipeout-delete">删除</a>
-          </div>
-        </li>
-        <li class="swipeout" @swipeout:deleted="lock">
-          <div class="swipeout-content section branch">
-            <div>
-              <a href="javascript:;" class="_icon _bankicon_3 link color-black">
-                <i class="icon f7-icons"> home </i>
-              </a>
-              <span class="type">招商银行储蓄卡</span>
-              <em>● ● ● ● 4113</em>
-            </div>
-            <span class="date">2016/10/15</span>
-          </div>
-          <div class="swipeout-actions-right">
-            <a href="#" data-confirm="银行卡锁定后，不能新增或删除银行卡，客服亦无权限操作，请谨慎操作银行卡锁定后，不能新增或删除银行卡，客服亦无权限操作，请谨慎操作！" data-confirm-title="温馨提示" class="swipeout-delete">锁定</a>
-          </div>
-        </li>
-        <li>
-          <div class="section add" @click="add">
-            <a href="javascript:;" class="_icon _plus link color-black">
-              <i class="icon f7-icons"> home </i>
-            </a><br />
-            <span>添加银行卡</span>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </f7-page>
+<template lang="pug">
+  f7-page.bank
+    f7-navbar(title="银行卡" back-link)
+    div.list
+      ul
+        li.swipeout(v-for=" u in userBankCards " :id=" u.entry " @swipeout:deleted="unbind")
+          div.swipeout-content.section.branch
+            div
+              a._icon.link.color-black(href="javascript:;" :class="'_bankicon_' + u.apiName")
+                i.icon.f7-icons home
+              span.type.c_3.ft_16 {{ u.bankName }}
+              em.ft_20.hlh_24 {{ u.cardNo }}
+            span.date.c_9.lh_40.ft_14 {{ u.addTime.split(' ')[0] }}
+          div.swipeout-actions-right
+            a.swipeout-delete(href="#" data-confirm="银行卡解绑后，不能再绑定到其它帐号上！" data-confirm-title="温馨提示") 删除
+        //- <li class="swipeout" @swipeout:deleted="unbind">
+        //-   <div class="swipeout-content section branch">
+        //-     <div>
+        //-       <a href="javascript:;" class="_icon _bankicon_2 link color-black">
+        //-         <i class="icon f7-icons"> home </i>
+        //-       </a>
+        //-       <span class="type">农业银行储蓄卡</span>
+        //-       <em>● ● ● ● 4113</em>
+        //-     </div>
+        //-     <span class="date">2016/10/15</span>
+        //-   </div>
+        //-   <div class="swipeout-actions-right">
+        //-     <a href="#" data-confirm="银行卡解绑后，不能再绑定到其它帐号上！" data-confirm-title="温馨提示" class="swipeout-delete">删除</a>
+        //-   </div>
+        //- </li>
+        //- <li class="swipeout" @swipeout:deleted="lock">
+        //-   <div class="swipeout-content section branch">
+        //-     <div>
+        //-       <a href="javascript:;" class="_icon _bankicon_3 link color-black">
+        //-         <i class="icon f7-icons"> home </i>
+        //-       </a>
+        //-       <span class="type">招商银行储蓄卡</span>
+        //-       <em>● ● ● ● 4113</em>
+        //-     </div>
+        //-     <span class="date">2016/10/15</span>
+        //-   </div>
+        //-   <div class="swipeout-actions-right">
+        //-     <a href="#" data-confirm="银行卡锁定后，不能新增或删除银行卡，客服亦无权限操作，请谨慎操作银行卡锁定后，不能新增或删除银行卡，客服亦无权限操作，请谨慎操作！" data-confirm-title="温馨提示" class="swipeout-delete">锁定</a>
+        //-   </div>
+        //- </li>
+        li
+          div.section.add(@click="add")
+            a._icon._plus.link.color-black(href="javascript:;")
+              i.icon.f7-icons home
+            br
+            span 添加银行卡
 </template>
 
 <script>
   import config from '@/config'
+  import api from '@/api'
   export default {
     mixins: [config],
     components: {
@@ -70,11 +61,23 @@
     name: 'bank',
     props: [],
     data () {
-      return {}
+      return {
+        userBankCards: [],
+        bankCardNum: 0,
+        lockCrad: 0
+      }
     },
     created () {
+      this.getUserBankCards()
     },
     methods: {
+      getUserBankCards () {
+        this.$.get(api.getUserBankCards).then(({data}) => {
+          // console.log(data.userBankCards)
+          this.userBankCards = data.userBankCards
+          // console.log(this.userBankCards)
+        })
+      },
       add () {
         this.__go('/me/bank/add/')
       },
@@ -117,20 +120,17 @@
             .type
               position relative
               top -0.06rem
-              font-size 0.29rem
-              color #333
             em
               position absolute
               right 0.4rem
-              font-size 0.36rem
               font-style normal
             .date
               position absolute
               right 0.4rem
-              font-size 0.22rem
-              color #999
         .swipeout-actions-left, .swipeout-actions-right
           width 1.5rem
           top 0.22rem
           height 1.82rem
+.bank .list .icon
+  background-size auto 80%
 </style>
