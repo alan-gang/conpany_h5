ca1ad3afc5775472baf538296f0f6240a41a31f6
joey <joey.huang@tech-core.org>
2019-08-16 18:13:46 +0800

    号号

--- src/views/rfs/_ac/index.vue
+++ src/views/rfs/_ac/index.vue
@@ -8,7 +8,7 @@ f7-page.rf_ac_(:page-content="false")
       //- .o_h(:class=" !rns_ ? 'mh_0' : 'mh_100' " style="transition: max-height .5s linear")
       f7-button.inlb(v-for=" (x, i) in stbs " :key="i" @click=" stbi = i " :color=" i === stbi ? 'deeporange' : 'gray' ") {{ x.n }}
 
-    f7-searchbar(ref="s" disable-button-text="取消" placeholder="输入账变编号号 或 用户名查询" :clear-button="true" @searchbar:disable=" rns_ = false " @change=" (rns_ = false) || (n = $event.target.value) " :value=" (!rns_ && n ? stbs[stbi].n + '：' : '') + n " @input=" (rns_ = true) && (n_ = $event.target.value) " @focus=" rns_ = true ")
+    f7-searchbar(ref="s" disable-button-text="取消" placeholder="输入账变编号 或 用户名查询" :clear-button="true" @searchbar:disable=" rns_ = false " @change=" (rns_ = false) || (n = $event.target.value) " :value=" (!rns_ && n ? stbs[stbi].n + '：' : '') + n " @input=" (rns_ = true) && (n_ = $event.target.value) " @focus=" rns_ = true ")
 
     template(v-if=" rns_ && !stbi ")
       .searchbar-backdrop.h_0(@click=" rns_ = false ")
