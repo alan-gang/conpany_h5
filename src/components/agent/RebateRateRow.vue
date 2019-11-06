b129ab9c9e514c05fef75b22f89b31112951e151
joey <joey.huang@tech-core.org>
2019-11-06 15:49:50 +0800

    1.开户调点增加滑杆

--- src/components/agent/RebateRateRow.vue
+++ src/components/agent/RebateRateRow.vue
@@ -3,22 +3,44 @@
     f7-row.rebate-type-row
       f7-col(width="20") 
         span(class="c_orange") {{item.name || item.groupName}} 
-      f7-col.ft_15(width="80") 
+      f7-col.ft_15(width="60") 
         span {{item.rebateTypeTxt}}
         span(class="c_orange") {{item.$ || 0.00}}{{item.unitSymbal}} 
         span.c_g &nbsp;({{item.unitTxt}}{{item.$ || 0.00}})
-    .rebate-swiper-wp(v-show="!isHighestRate")
-      slot(name="left")
-      slot(name="leftIcon")
-        .left-r(@click="swiperLeft(i)")
-      f7-swiper.rebate-swiper(:params="swiperParams" v-show="item.$s" ref="rebateSwiper")
-        f7-swiper-slide(@click.native="rebateItemHandler(0, i, 0)" :class="{active: 0 === item.$ * 1}") 0.0{{item.unitSymbal}}
-        f7-swiper-slide(v-for="(rate, j) in (user.roleId === 1 ? [item.$s] : item.$s)" :key="j" @click.native="rebateItemHandler(rate, i, j)" :class="{active: (rate * 0.1).toFixed(1) === item.$}") {{(rate * 0.1).toFixed(1)}}{{item.unitSymbal}}
 
-      slot(name="rightIcon")
-        .right-r(@click="swiperRight(i)")
-      slot(name="right")
-    .highest-tip.pt_15.pb_10(v-if="swiperParams.checkHighestRate && isHighestRate") 当前已是最高，不可再调整
+      f7-col(width="20")
+        f7-icon(f7="arrow_up_arrow_down_square_fill")
+
+    template(v-if=" isHighestRate ")
+      .highest-tip.pt_15.pb_10(v-if="swiperParams.checkHighestRate && isHighestRate") 当前已是最高，不可再调整
+
+    template(else)
+      template(v-show=" !showRange ")
+        .rebate-swiper-wp
+          slot(name="left")
+          slot(name="leftIcon")
+            .left-r(@click="swiperLeft(i)")
+          f7-swiper.rebate-swiper(:params="swiperParams" v-show="item.$s" ref="rebateSwiper")
+            f7-swiper-slide(@click.native="rebateItemHandler(0, i, 0)" :class="{active: 0 === item.$ * 1}") 0.0{{item.unitSymbal}}
+            f7-swiper-slide(v-for="(rate, j) in (user.roleId === 1 ? [item.$s] : item.$s)" :key="j" @click.native="rebateItemHandler(rate, i, j)" :class="{active: (rate * 0.1).toFixed(1) === item.$}") {{(rate * 0.1).toFixed(1)}}{{item.unitSymbal}}
+
+          slot(name="rightIcon")
+            .right-r(@click="swiperRight(i)")
+          slot(name="right")
+      
+      template(v-show=" showRange ")
+        //- 滑竿
+        .pb_10(v-if=" ifRange " v-show=" showRange ")
+          f7-row(no-gap)
+            f7-col(width="10")
+              .r_5.o_h.w_40.color-theme-gray(@click=" $f7.range.setValue('#point_range_adduser', pp) ")
+               .stepper-button-minus.h_30
+            f7-col.pl_15.pr_15(width="80")
+              f7-range#point_range_adduser(min="0" :max=' item.$s ' :step='0.1' :value='item.$ * 100' @range:change=" $emit('adjust:point', ($event / 100)._f3() * 1 ) ")
+            f7-col(width="10")
+              .r_5.o_h.w_40.color-theme-gray(@click=" $f7.range.setValue('#point_range_adduser', np) ")
+                .stepper-button-plus.h_30
+
 </template>
 
 <script>
@@ -51,15 +73,14 @@ export default {
   },
   data () {
     return {
-      isHighestRate: false // 是否启用最高比率显示，1.只显示最高比较文案，2.比率不可调整
+      isHighestRate: false, // 是否启用最高比率显示，1.只显示最高比较文案，2.比率不可调整
+      showRange: false,
     }
   },
-  watch: {
-    // item () {
-    //   this.$nextTick(() => {
-    //     this.updateSwiperPosition()
-    //   })
-    // }
+  computed: {
+    ifRange () {
+      return !this.item.groupId && !this.item.groupid
+    }
   },
   mounted () {
     if (this.swiperParams.checkHighestRate && this.item.$ * 10 === this.item.$s) this.isHighestRate = true
